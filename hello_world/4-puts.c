6c50e29a856c10581927db4794da22c35a9cef1e
UrbanoArthur <artur.urbano33@gmail.com>
2024-06-18 14:01:25 +0000

    k

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /*Commentary*/
 {
-puts("Programming is like building a multilingual puzzle")
-return (0)
+main(void);
+puts("Programming is like building a multilingual puzzle");
+return (0);
 }
