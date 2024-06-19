3239b580eec3a0bb9eb3a456a49a44551fe805d7
UrbanoArthur <arthur.urbano33@gmail.com>
2024-06-19 08:26:20 +0000

    k

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
- * main - prints exactly "with proper grammar, but
- *  the outcome is a piece of art,"
+ * main - prints exactly with proper grammar, but
+ *  the outcome is a piece of art,
  *
  * return: 0
  */
 int main(void)
 {
+
 printf("with proper grammar, but the outcome is a piece of art,");
 return (0);
 }
