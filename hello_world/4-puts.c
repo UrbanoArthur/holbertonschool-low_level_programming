86e31921a7f86eeb53e4193ad0aad6b989bd81fd
UrbanoArthur <arthur.urbano33@gmail.com>
2024-06-19 07:45:20 +0000

    j

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-/*Commentary*/
+/**
+ * main - prints exactly "Programming is like building
+ * a multilingual puzzle"
+ *
+ * Return: 0
+ */
+
+int main(void)
 {
-main(void);
-puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
