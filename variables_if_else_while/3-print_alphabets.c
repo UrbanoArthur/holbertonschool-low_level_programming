8f150e49ce73af94270b25c48aaa33bd463cf389
UrbanoArthur <arthur.urbano33@gmail.com>
2024-06-20 21:13:09 +0000

    l

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - ooo
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int c;
+int d;
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+for (d = 'A'; d <= 'Z'; d++)
+{
+putchar(d);
+}
+putchar('\n');
+return (0);
+}
