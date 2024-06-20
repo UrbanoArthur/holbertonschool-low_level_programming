11cdfd1cb281c1c67f99e2955a9637d2a22bfc5c
UrbanoArthur <arthur.urbano33@gmail.com>
2024-06-20 21:59:21 +0000

    l

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - ooo
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int base10;
+for (base10 = 0; base10 <= 9; base10++)
+{
+printf("%d", base10);
+}
+putchar('\n');
+return (0);
+}
