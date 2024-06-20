cec0747a054c4673503f66fa6242f909399cb99f
UrbanoArthur <arthur.urbano33@gmail.com>
2024-06-20 12:39:16 +0000

    k

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -14,13 +14,13 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n < 0)
-		printf("is negative");
+		printf("%d is negative\n", n);
 
 	else if (n == 0)
-		printf("is zero");
+		printf("%d is zero\n", n);
 
 	else
-		printf("is positive");
+		printf("%d is positive\n", n);
 
 	return (0);
 }
