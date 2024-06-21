fed123237f301332e5778be7bc3567c4d85c40a7
UrbanoArthur <arthur.urbano33@gmail.com>
2024-06-21 12:03:34 +0000

    j

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 /**
- * main - ooo
- *
- * Return: 0
- */
+* main - ooo
+*
+* Return: 0
+*/
 int main(void)
 {
-	int c = [0-9];
-	int d = [a-f];
+int c;
+char d;
+for (c = '0'; c <= '9'; c++)
+{
+putchar(c);
+}
+for (d = 'a'; d <= 'f'; d++)
+{
+putchar(d);
+}
+putchar('\n');
+return (0);
+}
