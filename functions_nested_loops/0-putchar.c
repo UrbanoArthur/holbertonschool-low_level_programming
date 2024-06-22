f8a0a102df68b772d07f5c640a6f436db33cfedb
UrbanoArthur <arthur.urbano33@gmail.com>
2024-06-22 22:18:13 +0000

    l

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - ooo
+*
+* Return: 0
+*/
+int main(void)
+{
+	char n[] = "_putchar";
+	int a = 0;
+	{
+		for (a = 0; a < 9; a++)
+		{
+		putchar(n[a]);
+		}
+		putchar('\n');
+	}
+return (0);
+}
