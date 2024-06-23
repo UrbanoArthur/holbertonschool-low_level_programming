97b0484e26817aa9126ae8977f2dee7d80f8d0ef
UrbanoArthur <arthur.urbano33@gmail.com>
2024-06-23 21:35:38 +0000

    m

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - ooo
+ *
+ *@c: ooo
+ * Return: 0
+ */
+int _abs(int n)
+{
+	{
+	if (n < 0)
+	{
+	return (n * -1);
+	}
+	else
+	{
+		return (n);
+	}
+}
+}
