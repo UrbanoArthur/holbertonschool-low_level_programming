281660406cea9f30d8191d51e55f45a721ee87fb
UrbanoArthur <arthur.urbano33@gmail.com>
2024-06-23 20:14:10 +0000

    m

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+/**
+ * main - ooo
+ * print_alphabet - ooo
+ * int _islower - ooo
+ * c - oo
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
