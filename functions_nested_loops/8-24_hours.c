528cefd6417d9597cea498e0f9238a5547b3574d
UrbanoArthur <arthur.urbano33@gmail.com>
2024-06-23 22:14:58 +0000

    m

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -7,18 +7,18 @@
  */
 void jack_bauer(void)
 {
-	int h = 0;
-	int m = 0;
-	for (h = 0; h < 24; h++)
-	{
-		for (m = 0; m < 60; m++)
-	{
-		_putchar('0' + h/10);
-		_putchar('0' + h % 10);
-		_putchar(':');
-		_putchar('0' + m/10);
-		_putchar('0' + m % 10);
-		_putchar('\n');
-	}
+int h = 0;
+int m = 0;
+for (h = 0; h < 24; h++)
+{
+for (m = 0; m < 60; m++)
+{
+_putchar('0' + h / 10);
+_putchar('0' + h % 10);
+_putchar(':');
+_putchar('0' + m / 10);
+_putchar('0' + m % 10);
+_putchar('\n');
+}
 }
 }
