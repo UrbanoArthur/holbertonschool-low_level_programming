18c6aa676420a09e7d50f5a4659a1a90622911b6
UrbanoArthur <arthur.urbano33@gmail.com>
2024-06-23 22:18:14 +0000

    m

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -7,11 +7,9 @@
  */
 int print_last_digit(int r)
 {
-    int last_digit = r % 10;
-
-    if (last_digit < 0) {
-	    last_digit = -last_digit;
-    }
-    _putchar(last_digit);
-    return last_digit;
+int last_digit = r % 10;
+if (last_digit < 0)
+last_digit = -last_digit;
+_putchar(last_digit);
+return (last_digit);
 }
