e2f3b99298cb6125a7fdfce6a9ccb56c8bdf629e
UrbanoArthur <arthur.urbano33@gmail.com>
2024-06-25 15:56:56 +0000

    m

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *more_numbers - ooo
+ */
+void more_numbers(void)
+{
+int i;
+int j;
+
+{
+for (j = 0; j <= 10; j++)
+{
+for (i = 0; i <= 14; i++)
+{
+if (i >= 10)
+_putchar('1');
+_putchar((i % 10) + '0');
+}
+_putchar('\n');
+}
+}
+}
