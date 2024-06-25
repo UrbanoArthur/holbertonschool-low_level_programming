7687eec889e73a8f9c64e59dcf9a49f10dc02a56
UrbanoArthur <arthur.urbano33@gmail.com>
2024-06-25 15:28:59 +0000

    m

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_most_numbers - ooo
+ */
+void print_most_numbers(void)
+{
+int i;
+{
+	for (i = '0'; i <= '9'; i++)
+		if (i != '2' && i != '4')
+			_putchar(i);
+}
+_putchar('\n');
+}
