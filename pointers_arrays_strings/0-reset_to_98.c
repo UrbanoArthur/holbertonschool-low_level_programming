abf61cf2ac4fcf0c8dbe329a04f82ec88f7db0b3
UrbanoArthur <arthur.urbano33@gmail.com>
2024-06-26 19:04:42 +0000

    w

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ *
+ *
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,4 @@
+#ifndef URMOM
+#define URMOM
+void reset_to_98(int *n);
+#endif
