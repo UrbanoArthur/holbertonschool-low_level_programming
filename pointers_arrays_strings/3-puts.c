8abbe4258cafeed9afcb1c1fb5d6e1e9ee501624
UrbanoArthur <arthur.urbano33@gmail.com>
2024-06-28 12:15:16 +0000

    m

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - ooo
+ * @str: ooo
+ *
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		{
+			str++;
+		}
+	}
+       	_putchar('\n');
+}
