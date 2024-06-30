b4120502cb52751f8db808d5fc244fd45d0e2b86
UrbanoArthur <arthur.urbano33@gmail.com>
2024-06-30 14:57:42 +0000

    p

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ *
+ */
+void puts2(char *str)
+
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar (str[i]);
+		}
+	}
+	_putchar ('\n');
+}
