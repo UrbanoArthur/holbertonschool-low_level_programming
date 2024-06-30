4fdb299abe8e62ed3c593db860d13921dbe458ec
UrbanoArthur <arthur.urbano33@gmail.com>
2024-06-30 21:13:46 +0000

    m

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strcat - ooo
+ * @dest: ooo
+ * @src: ooo
+ * Return: ooo
+ *
+ *
+ */
+char *_strcat(char *dest, char *src)
+
+{
+	int j = 0;
+	int i = 0;
+
+	while (dest[j] != '\0')
+	{
+		j++;
+	}
+	while (src[i] != '\0')
+	{
+		j++;
+		i++;
+	}
+	dest[j] = '\0';
+	return (dest);
+}
