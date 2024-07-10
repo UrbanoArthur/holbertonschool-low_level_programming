24ab1a6411b13174518b6ba2a5d2c338cea5cf0e
UrbanoArthur <arthur.urbano33@gmail.com>
2024-07-10 20:48:03 +0000

    m

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * @size:
+ * @c:
+ * create_array - 
+ */
+char *create_array(unsigned int size, char c)
---
+++ malloc_free/main.h
@@ -0,0 +1,4 @@
+#ifndef URMOM
+#define URMOM
+char *create_array(unsigned int size, char c);
+#endif
