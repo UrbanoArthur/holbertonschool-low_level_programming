c86b36f9e54a55166b9a4b40420ffec9f7eac239
UrbanoArthur <arthur.urbano33@gmail.com>
2024-07-10 21:59:39 +0000

    p

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -7,3 +7,19 @@
  * create_array - 
  */
 char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *p;
+
+	if (size == 0)
+        {
+                return (NULL);
+        }	
+	p = malloc(size * sizeof(char));
+
+	for (i = 0; i < size; i++)
+	{
+		p[i] = c;
+	}
+	return (p);
+}
