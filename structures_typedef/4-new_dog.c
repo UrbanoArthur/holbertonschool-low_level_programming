ab1e494ce6094527655e11f915475a628ac0d71b
UrbanoArthur <arthur.urbano33@gmail.com>
2024-07-12 21:54:46 +0000

    p

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+/**
+ * new_dog - ooo
+ * @name: ooo
+ * @age: ooo
+ * @owner: ooo
+ * Return: ooo
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog_t d;
+
+	d = malloc
+
+	d = {
+		name = name,
+		age = age,
+		owner = owner,
+	}
+
+	if (d == NULL)
+        {
+                return (NULL);
+        }
+	return (d);
+}
+
