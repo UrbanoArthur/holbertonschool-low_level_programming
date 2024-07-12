aeea639279e025b009c1c89a7fb276723a25e778
UrbanoArthur <arthur.urbano33@gmail.com>
2024-07-12 14:11:45 +0000

    p

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -3,15 +3,21 @@
 #include <limits.h>
 #include "main.h"
 /**
+ * malloc_checked - ooo
+ * @b: ooo
+ *
+ * Return: ooo
  *
  */
 void *malloc_checked(unsigned int b)
 {
-	int *ptr;
-	ptr = malloc (sizeof(void *) * b);
+	void *ptr;
+
+	ptr = malloc(b);
+
 	if (ptr == NULL)
 	{
-		return ("98");
+		exit(98);
 	}
 	return (ptr);
 }
