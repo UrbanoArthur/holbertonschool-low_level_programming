e731117016edca53fdbef12da235dde8278ddcf0
UrbanoArthur <arthur.urbano33@gmail.com>
2024-07-12 00:43:43 +0000

    p

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,12 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "main.h"
 /**
  * @str: ooo
+ *
  * _strdup - ooo
+ *
  * Return: ooo
  */
 char *_strdup(char *str)
 
 {
+	char *ptr;
+	size_t len;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	len = strlen(str);
+	ptr = (char *)malloc((len + 1) * sizeof(char));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(ptr, str);
+	return (ptr);
+}
 
