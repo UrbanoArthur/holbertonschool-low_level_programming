f1233fb0a26f73780a1a8562cfdfad8583345fd1
UrbanoArthur <arthur.urbano33@gmail.com>
2024-07-12 10:31:59 +0000

    p

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ *
+ */
+void free_grid(int **grid, int height)
+{
+	int a;
+
+		if (grid == NULL || height <= 0)
+		{
+			return;
+		}
+		for (a = 0; a < height; a++)
+		{
+			free(grid[a]);
+		}
+		free(grid);
+}	
