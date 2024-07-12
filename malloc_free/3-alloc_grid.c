cfe3ae7d8002a605c803ae1d71b0218a93fd9e54
UrbanoArthur <arthur.urbano33@gmail.com>
2024-07-12 10:06:14 +0000

    y

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * alloc_grid - ooo
+ * @width: ooo
+ * @height: ooo
+ * Return: ooo
+ */
+int **alloc_grid(int width, int height)
+{
+	int **ptr;
+	int a, b;
+
+		if (width <= 0 || height <= 0)
+		{
+			return (NULL);
+		}
+
+	ptr = (int **)malloc(sizeof(int *) * height);
+
+		if (ptr == NULL)
+		{
+			return (NULL);
+		}
+	for (a = 0; a < height; a++)
+	{
+		ptr[a] = malloc(sizeof(int) * height);
+	}
+	if (ptr[a] == NULL)
+	{
+		b = 0;
+		while (a > b)
+		{
+			free(ptr[b]);
+			b++;
+		}
+		free(ptr);
+		return (NULL);
+	}
+	for (b = 0; b < width; b++)
+	{
+		ptr[a][b] = 0;
+	}
+	return (ptr);
+}
