9282c28778d0d6307f801d5851f629a60fa6ce7c
UrbanoArthur <arthur.urbano33@gmail.com>
2024-07-12 21:16:54 +0000

    m

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include "dog.h"
 /**
- *
+ * init_dog - ooo
+ * @d: ooo
+ * @name: ooo
+ * @age: ooo
+ * @owner: ooo
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
+
 {
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
-	return;
 }
