94e966799a26549a1c19a3e031e38efdd2e97375
UrbanoArthur <arthur.urbano33@gmail.com>
2024-07-17 12:07:34 +0000

    p

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include "function_pointers.h"
 /**
- *
+ * print_name - la fonction
+ * @name: les noms a afficher
+ * @f: le pointeur vers la fonction print name
+ * Return: rien
  */
 void print_name(char *name, void (*f)(char *))
 {
-	void (*ptr)(char, void, char);
-	ptr = &print_name;
-	return (ptr);
+	if (name && f)
+	{
+		f(name);
+	}
 }
