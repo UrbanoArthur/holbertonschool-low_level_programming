28257e77073a41897ec52cdbd58d21dbfcc1dc11
UrbanoArthur <arthur.urbano33@gmail.com>
2024-07-19 21:58:21 +0000

    p

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 {
 	int num1, num2, result;
 	int (*op_func)(int, int);
-	
+
 	if (argc != 4)
 	{
 	printf("Error\n");
@@ -15,7 +15,8 @@ int main(int argc, char *argv[])
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 	op_func = get_op_func(argv[2]);
-	if (op_func == NULL) 
+
+	if (op_func == NULL)
 	{
 	    printf("Error\n");
 	    return (99);
