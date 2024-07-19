0edd12953220bd29b3a88a6f4e894361844244ad
UrbanoArthur <arthur.urbano33@gmail.com>
2024-07-19 21:54:40 +0000

    m

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+/**
+ * op_add - plus
+ * op_sub - moins
+ * op_mul - multiplier
+ * op_div - diviser
+ * op_mod - reste de la division
+ * Return: ooo
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
